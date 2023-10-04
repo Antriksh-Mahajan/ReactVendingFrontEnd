faf18e6024f80031a08b138c78ee27571d033da4
Antriksh2000 <antrikshmahajan5@gmail.com>
2023-10-04 17:49:37 +0530

    04-10-2023 added loading attribute in img

--- src/Components/MaterialTailwind.jsx
+++ src/Components/MaterialTailwind.jsx
@@ -31,6 +31,7 @@ export function MaterialTailwind() {
               src={post.acf.postimage}
               alt="image 1"
               className="h-full w-full object-cover"
+              loading="lazy"
             />
             <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
               <div className="w-3/4 text-center md:w-2/4">
